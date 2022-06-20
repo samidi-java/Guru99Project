fe0150b2edbc523fe628f91495e773d5d9448685
samidi-java <samidi.java@gmail.com>
2022-06-20 08:37:16 +0530

    added the new  class Test1.java

---
+++ src/test/java/testCases/Test1.java
@@ -0,0 +1,18 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("in test1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("in test2");
+	}
+}
